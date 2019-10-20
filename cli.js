418bd55f9a62da529b099763565a7bc5823a60b6
Cokorda Raka Angga Jananuraga <r.cokorda@svitla.com>
2019-10-20 16:19:04 -0500

    Added CLI

---
+++ cli.js
@@ -0,0 +1,11 @@
+const stdin = process.openStdin();
+const calculator = require('./index');
+
+process.stdout.write("? ");
+stdin.addListener("data", data => {
+  calculator.calculatePromise(data.toString().trim()).then(result => {
+    console.log(`= ${result}`);
+
+    process.stdout.write("? ");
+  }).catch(error => console.log("! ", error));
+});
